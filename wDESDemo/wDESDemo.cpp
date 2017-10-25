c5bb22a274b395688bffd4ddc6a61d28d275d63e
Huang-Jin <1309740625@qq.com>
2017-10-25 17:00:56 +0800

    2/3

--- wDESDemo/wDESDemo.cpp
+++ wDESDemo/wDESDemo.cpp
@@ -43,6 +43,29 @@ int gCompressTransform[48] =
 
 int gKeyCircleShift[ROUND_NUM] = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
 
+int gExtentedTransform[48] =
+{
+	32, 1, 2, 3, 4, 5,
+	4, 5, 6, 7, 8, 9,
+	8, 9, 10, 11, 12, 13,
+	12, 13, 14, 15, 16, 17,
+	16, 17, 18, 19, 20, 21,
+	20, 21, 22, 23, 24, 25,
+	24, 25, 26, 27, 28, 29,
+	28, 29, 30, 31, 32, 1
+};
+
+int gSBox[8][64]=
+{
+	{ 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7, 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8, 4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0, 15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 },
+	{ 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10, 3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5, 0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15, 13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 },
+	{ 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8, 13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1, 13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7, 1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 },
+	{ 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15, 13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 19, 10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4, 3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 },
+	{ 2, 12, 4, 1, 7, 10, 11, 6, 5, 8, 3, 15, 13, 0, 14, 9, 14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 13, 3, 9, 8, 6, 4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14, 11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 },
+	{ 12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11, 10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8, 9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6, 4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 },
+	{ 4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1, 13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6, 1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2, 6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 },
+	{ 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7, 1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2, 7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8, 2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 }
+};
 
 // get bits array(0,1) from a char array
 int *getBits(char *data, int dataLen)
@@ -88,9 +111,60 @@ char *fromBits(int * bits, int len)
 	return data;
 }
 
-char ** getRoundKeys(char *key, int roundNum)
+// copy bits from src to a new array with length len.
+int *copyBits(int *src, int len)
 {
-	char **roundKeys = new char*[roundNum];
+	int* cpBits = new int[len];
+
+	try
+	{
+		for (int i = 0; i < len; i++)
+		{
+			cpBits[i] = src[i];
+		}
+	}
+	catch (int)
+	{
+		delete[] cpBits;
+		return NULL;
+	}
+
+	return cpBits;
+}
+
+// merge left and right into a whole part
+int *mergeBits(int *left, int leftLen, int *right, int rightLen)
+{
+	int *merged = new int[leftLen + rightLen];
+
+	for (int i = 0; i < leftLen; i++)
+	{
+		merged[i] = left[i];
+	}
+	for (int i = 0; i < rightLen; i++)
+	{
+		merged[leftLen + i] = left[i];
+	}
+
+	return merged;
+}
+
+// xor in bits
+int *xorBits(int *bits1, int *bits2, int len)
+{
+	int *xoredBits = new int[len];
+
+	for (int i = 0; i < len; i++)
+	{
+		xoredBits[i] = bits1[i] ^ bits2[i];
+	}
+
+	return xoredBits;
+}
+
+int ** getRoundKeys(char *key, int roundNum)
+{
+	int **roundKeys = new int*[roundNum];
 	//for (int i = 0; i < roundNum; i++)
 	//{
 	//	roundKeys[i] = new char[XOR_KEY_LENGTH];
@@ -151,9 +225,9 @@ char ** getRoundKeys(char *key, int roundNum)
 			keyBits[j] = shiftedBits[gCompressTransform[j] - 1];
 		}
 
-		roundKeys[i] = fromBits(keyBits, 48);
+		roundKeys[i] = keyBits;
 
-		delete[] keyBits;
+		//delete[] keyBits;
 		delete[] temp;
 		delete[] transformedBits;
 		transformedBits = shiftedBits;
@@ -173,6 +247,39 @@ int * IPTransform(char *data, int dLen)
 	}
 
 	return transformedBits;
+}
+
+int * ExtentedTransform(int *bits, int transformLen)
+{
+	int *transformedBits = new int[transformLen];
+
+	for (int i = 0; i < transformLen; i++)
+	{
+		transformedBits[i] = bits[gExtentedTransform[i] - 1];
+	}
+
+	return transformedBits;
+}
+
+int * SBoxTransform(int *bits)
+{
+	int *transfomedBits = new int[32];
+
+	// eight boxes
+	for (int i = 0; i < 8; i++)
+	{
+		int row = bits[i * 6] & bits[i * 6 + 5];
+		int rank = bits[i * 6 + 1] & bits[i * 6 + 2] & bits[i * 6 + 3] & bits[i * 6 + 4];
+
+		int temp = gSBox[i][row * 16 + rank];
+
+		for (int j = 0; j < 4; j++)
+		{
+			transfomedBits[i * 4 + j] = (temp >> (4 - j - 1)) & 1;
+		}
+	}
+
+	return transfomedBits;
 }
 
 char * encryptDes(char *src, int srcLen, char *key, int keyLen)
@@ -199,7 +306,7 @@ char * encryptDes(char *src, int srcLen, char *key, int keyLen)
 	}
 	char * encryptedData = new char[encryptedDataLen];
 
-	char **roundKeys = getRoundKeys(key,ROUND_NUM);
+	int **roundKeys = getRoundKeys(key,ROUND_NUM);
 	if (roundKeys == NULL)
 	{
 		return NULL;
@@ -211,8 +318,8 @@ char * encryptDes(char *src, int srcLen, char *key, int keyLen)
 	for (int i = 0; i < encryptBlockNum; i++)
 	{
 		char *blockData = new char[BLOCK_LENGTH];
-		char *leftPart;
-		char *rightPart;
+		int *leftPart;
+		int *rightPart;
 
 		for (int j = 0; j < BLOCK_LENGTH; j++)
 		{
@@ -223,11 +330,23 @@ char * encryptDes(char *src, int srcLen, char *key, int keyLen)
 
 		for (int j = 0; j < ROUND_NUM; j++)
 		{
-			char *roundkey = roundKeys[j];
+			int *roundkey = roundKeys[j];
 
+			leftPart = copyBits(blockBits,32); // with length of 32
+			rightPart = ExtentedTransform(blockBits + BLOCK_LENGTH*BYTE_TO_BIT / 2, 48);
 
+			int *xoredBits = xorBits(roundkey, rightPart, 48);
+			int *sBoxTransformed = SBoxTransform(xoredBits);
 
+			//pbox
+
+			int *finalxoredBits = xorBits(sBoxTransformed, leftPart, 32);
+			
+			delete[] blockBits;
+			blockBits = mergeBits(rightPart, 32, finalxoredBits, 32);
 		}
+
+		// ip transpose
 
 		delete[] blockData;
 	}
